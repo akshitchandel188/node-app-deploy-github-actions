9bf29fc55f4975f4b8cf0171812afce0fa3d117a
Akshit <rshallu146@gmail.com>
2026-03-21 05:13:38 +0530

    finally updating

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-    return res.json({
-  msg: "Hello World from new msg",
-  version: "v3 🚀",
-  
-});
+    return res.json({msg: "Finally updating the message"});
 })
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
