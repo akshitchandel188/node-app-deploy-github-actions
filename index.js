ed3c5678c0c2f0effa4ad5a3805ec3c3e8e1d167
Akshit <rshallu146@gmail.com>
2026-03-21 04:34:31 +0530

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-    return res.json({ msg: 'Hello World! from hostel' })
+    return res.json({ msg: 'Hello World! from hostel , new changes are being made' })
 })
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
