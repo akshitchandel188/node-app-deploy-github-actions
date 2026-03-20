08c38ae1fdcd4057a939c3684c73e9c3857c6417
Akshit <rshallu146@gmail.com>
2026-03-21 04:59:36 +0530

    force error test

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-    throw new Error("FORCE ERROR TEST 🚀");
+    return res.json({
+  msg: "Hello World",
+  version: "v3 🚀",
+  time: new Date()
 });
+})
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 })
